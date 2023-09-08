9bc519e1cc2ea6075ceeac67b0040933a52d4194
Md Ahsan Habib <63815890+habib671@users.noreply.github.com>
2023-09-08 19:27:49 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,80 +1,75 @@
-<!DOCTYPE HTML>
-<html lang="en-US">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css" media="all" />
-	<style>
-		body{
-	background-color: grey;
-}
-
-form{
-	width:350px;
-	
-	padding: 20px;
-	margin: 10% auto;
-	box-sizing: border-box;
-	box-shadow: 0 0 5px 1px;
-}
-
-form input{
-	width: 100%;
-	padding-left: 10px;
-	margin: 8px 0;
-	box-sizing: border-box;
-	height:35px;
-	border-radius:6px;
-}
-
-form input[type="submit"]{
-	background:green;
-	color: #fff;
-	font-weight: bold;
-	border:none;
-	font-size:16px;
-	cursor:pointer;
-	
-}
-	</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        form{
+            width: 350px;
+            padding: 20px;
+            margin: 10% auto;
+            box-shadow: 0 0 5px 1px;
+            box-sizing: border-box;
+        }
+        form input{
+            width:100%;
+            height:35px;
+            box-sizing: border-box;
+            padding: 6px;
+            border-radius:5px;
+            margin: 7px 0;           
+        }
+        form input[type="submit"]{
+            background: green;
+            color: #fff;
+            font-size: 16px;
+            font-weight:bold;
+            cursor:pointer;
+        }
+    </style>
 </head>
 
-
 <?php
-   $connect = mysqli_connect('localhost', 'root', '', 'project');
 
-   if (isset($_POST['register'])){
+$connect = mysqli_connect('localhost','root','','project');
 
-    $name=$_POST['name'];
-    $username=$_POST['username'];
-    $email=$_POST['email'];
-    $password=$_POST['password'];
+if(isset($_POST['show'])){
+    header("Location: show.php");
+}
+if(isset($_POST['register'])){
+    $name = $_POST['name'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+$insert = "INSERT INTO data(name,username,email,password)
+VALUES('$name','$username','$email','$password')";
+    $query = mysqli_query($connect,$insert);
+    header("Location: success.php");
+}
 
-    $insert = "INSERT INTO data(name,username,email,password)
-    VALUES('$name','$username','$email','$password')";
-
-    $query= mysqli_query($connect,$insert);
-   }
 ?>
 
 
+
 <body>
-	<form method="POST">
-	
-	<label>Name</label>
-	<input placeholder="Name" type="text" name="name" />
-	
-	<label>Username</label>
-	<input type="text" name="username" />
-	
-	<label>Email</label>
-	<input type="email" name="email" />
-	
-	<label>Password</label>
-	<input type="password" name="password" />
-	
-	<input type="submit" value="Register" name="register">
-	
-	</form>
+<form action="" method="post">
+    <label>Name</label>
+    <input name="name" type="text"/>
+    <label >Username</label>
+    <input type="text" name="username">
+    <label >Email</label>
+    <input type="text" name="email">
+    <label >Password</label>
+    <input type="pasword" name="password">
+    <a href="C:\xampp\htdocs\show.php">
+    <input type="submit" value="Register" name="register">
+    </a>
+
+    <input type="submit" name="show" value="Show Table">
+</form>
+
+
+
 </body>
 </html>
